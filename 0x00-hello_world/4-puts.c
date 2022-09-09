6116d7240239f621dde64299536781a150ca87f6
chukwubuikem <ezeiruemma5t@gmail.com>
2022-09-08 18:16:04 -0700

    a C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - it prints programming is like building a multilingual puzzle
+ * return : 0 return zero
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}	
