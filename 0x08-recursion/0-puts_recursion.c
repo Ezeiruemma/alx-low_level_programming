867c822635486f5902604181826e9cbaa59c506c
chukwubuikem <ezeiruemma5t@gmail.com>
2022-09-28 19:07:58 -0700

    made chages to my code

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/*
+
+/**
  *_puts_recursion - prints a string
  *@:s
  *Return: 0
@@ -9,7 +10,7 @@ void _puts_recursion(char *s)
 {
 	if (*s == 0)
 	{
-		_putchar("\n");
+		_putchar('\n');
 		return;
 	}
 	_putchar(*s);
