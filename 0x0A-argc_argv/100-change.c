896ff2e9ba1430aff6b50085c87a43963f3c03b0
chukwubuikem <ezeiruemma5t@gmail.com>
2022-10-03 14:45:05 -0700

    a program that prints the minimum number of coins to make change for an amount of money

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,99 +1,28 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
-
-
+#include <ctype.h>
 /**
- *
- *   * main - prints the minimum number of coins to make change.
- *
- *     * @argc: argument counter.
- *
- *       * @argv: argument vector.
- *
- *         * Return: return 1 if error, else return 0.
- *
- *           */
-
+ * main - main funct
+ * @argc: param count
+ * @argv: param poiter
+ * Return: int
+ */
 int main(int argc, char *argv[])
-
 {
+	int i, j, sum = 0;
 
-		int change, num;
-
-
-
-			change = 0;
-
-
-
-				if (argc == 2)
-
-						{
-
-									num = atoi(argv[1]);
-
-											if (num > 0)
-
-														{
-
-																		for (; num >= 25; change++)
-
-																						{
-
-																											num = num - 25;
-
-																														}
-
-																					for (; num >= 10; change++)
-
-																									{
-
-																														num = num - 10;
-
-																																	}
-
-																								for (; num >= 5; change++)
-
-																												{
-
-																																	num = num - 5;
-
-																																				}
-
-																											for (; num >= 2; change++)
-
-																															{
-
-																																				num = num - 2;
-
-																																							}
-
-																														for (; num >= 1; change++)
-
-																																		{
-
-																																							num = num - 1;
-
-																																										}
-
-																																}
-
-													printf("%d\n", change);
-
-															return (0);
-
-																}
-
-					else
-
-							{
-
-										printf("Error\n");
-
-												return (1);
-
-													}
-
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
 }
